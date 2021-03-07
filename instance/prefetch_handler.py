22a19b7d184ebd3d1e3e7e17f80d1e0206e2ab02
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2021-03-07 15:08:58 +0100

    Measure hit ratio

--- instance/prefetch_handler.py
+++ instance/prefetch_handler.py
@@ -18,16 +18,25 @@ class PrefetchBufferHandler:
             'v2': pickle.load(open(f'./instance/model_files/em_v2_k{os.getenv("BUFFER_SEQ_LENGTH")}.pkl','rb')),
             'v4': pickle.load(open(f'./instance/model_files/em_v4_k{os.getenv("BUFFER_SEQ_LENGTH")}.pkl','rb'))
         }
+        self.n_queries = 0
+        self.n_hits = 0
 
     def __call__(self, *args):
-        print(f'[Prefetch_Handler] __call__ with args: {args}')
-        print(f'[Prefetch_Handler] Current buffer: {self.buffer.items()}')
+        # print(f'[Prefetch_Handler] __call__ with args: {args}')
+        self.n_queries += 1
+        print(f'[Prefetch_Handler] hit-ratio: {self.n_hits}/{self.n_queries} = {round((self.n_hits/self.n_queries)*100, 2)}%')
+        # print(f'[Prefetch_Handler] Current buffer: {self.buffer.items()}')
         if(os.getenv('ENABLE_PREFETCHING') == 'false'):
             return self.fn(*args)
         video, quality, tile, seg = self.get_video_segment_and_tile(args)
         resp = None
         if (video, seg) in self.buffer:
-            resp = self.buffer[(video, seg)][tile] if tile in self.buffer[(video, seg)] else self.fn(*args)
+            # resp = self.buffer[(video, seg)][tile] if tile in self.buffer[(video, seg)] else self.fn(*args)
+            if tile in self.buffer[(video, seg)]:
+                self.n_hits += 1 
+                resp = self.buffer[(video, seg)][tile]
+            else:
+                resp = self.fn(*args)
         else:
             resp = self.fn(*args)
             Thread(
@@ -51,20 +60,20 @@ class PrefetchBufferHandler:
         return int(video), int(quality), int(tile), int(seg)
     
     def prefetch(self, args, video, segment, tile, next_segment=False):
-        actual_segment = segment if not next_segment else segment + 1
+        actual_segment = segment - 1 if not next_segment else segment
         if self.buffer_keys.qsize() >= self.buffer_keys_len:
             first_key = self.buffer_keys.get()
             del self.buffer[first_key]
         # self.buffer[(video, actual_segment)] = self.manager.dict()
-        self.buffer[(video, actual_segment)] = dict()
-        self.buffer_keys.put((video, actual_segment))
-        pred_seg = self.prefetch_models[f'v{video}'].predict_next_segment(actual_segment - 1, tile) if next_segment else self.prefetch_models[f'v{video}'].predict_current_segment(actual_segment)
+        self.buffer[(video, actual_segment + 1)] = dict()
+        self.buffer_keys.put((video, actual_segment + 1))
+        pred_seg = self.prefetch_models[f'v{video}'].predict_next_segment(actual_segment - 1, tile - 1) if next_segment else self.prefetch_models[f'v{video}'].predict_current_segment(actual_segment)
         # print(pred_seg)
         for i_tile in pred_seg:
             pred_args = list(args)
             pred_args[4] = video
-            pred_args[6] = f'seg_dash_track{i_tile}_{actual_segment}.m4s'
-            self.buffer[(video, actual_segment)][i_tile] = self.fn(*tuple(pred_args))
+            pred_args[6] = f'seg_dash_track{i_tile + 1}_{actual_segment + 1}.m4s'
+            self.buffer[(video, actual_segment + 1)][i_tile + 1] = self.fn(*tuple(pred_args))
         # print(f'[prefetch] Current buffer: {[(k, v.items()) for k, v in self.buffer.items()]}')
 
 
