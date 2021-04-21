bf9c24de7800c6d24a86c82cbce92a1ca98a6284
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2021-04-21 17:47:39 +0200

    Prefetcher with threshold set to 90

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 if [ "$ENABLE_TC" == "true" ]
 then
     #CIP=$(getent hosts ${CLIENT_HOST} | cut -d' ' -f1)
-    echo "./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${EXPLORA_VR_CLIENT_SERVICE_HOST}"
-    ./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${EXPLORA_VR_CLIENT_SERVICE_HOST}
+    echo "./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CLIENT_HOST}"
+    ./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CLIENT_HOST}
 fi
 
 service redis-server start
