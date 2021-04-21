0f7f18e027374952751ee99db85a698459fecb57
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2021-04-21 16:53:22 +0200

    Update k8s deployment

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 if [ "$ENABLE_TC" == "true" ]
 then
-    CIP=$(getent hosts ${CLIENT_HOST} | cut -d' ' -f1)
-    echo "./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CIP}"
-    ./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CIP}
+    #CIP=$(getent hosts ${CLIENT_HOST} | cut -d' ' -f1)
+    echo "./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${EXPLORA_VR_CLIENT_SERVICE_HOST}"
+    ./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${EXPLORA_VR_CLIENT_SERVICE_HOST}
 fi
 
 service redis-server start
