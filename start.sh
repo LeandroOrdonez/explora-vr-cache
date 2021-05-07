3e2d377144020c9cd36cb05801fb7d82a6344c6c
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2021-05-07 21:42:52 +0200

    Update traffic control for supporting multiple clients

--- start.sh
+++ start.sh
@@ -2,9 +2,15 @@
 
 if [ "$ENABLE_TC" == "true" ]
 then
-    CIP=$(getent hosts ${CLIENT_HOST} | cut -d' ' -f1)
-    echo "./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CIP}"
-    ./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CIP}
+    for h in 1 2 3 4 5 6
+    do
+        CIP=$(getent hosts ${CLIENT_HOST}_${h} | cut -d' ' -f1)
+        echo "./traffic-control.sh -o --delay=${LATENCY} --jitter=${JITTER} --uspeed=${BANDWIDTH} --dspeed=${BANDWIDTH} ${CIP}"
+        tc qdisc add dev eth0 root handle 1a1a: htb default 1
+        tc class add dev eth0 parent 1a1a: classid 1a1a:${h} htb rate ${BANDWIDTH}
+        tc qdisc add dev eth0 parent 1a1a:${h} handle 220${h}: netem delay ${LATENCY}ms
+        tc filter add dev eth0 protocol ip parent 1a1a: prio 1 u32 match ip dst ${CIP} flowid 1a1a:${h}
+    done
 fi
 
 service redis-server start
